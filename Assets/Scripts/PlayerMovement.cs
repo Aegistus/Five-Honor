5a6be7d89c345056943eafa12d71607ec6f24b83
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-04-10 22:44:04 -0400

    Hide and Confine Cursor

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,12 @@ public class PlayerMovement : MonoBehaviour
     Vector3 movementVector;
     Quaternion targetRotation, currentRotation;
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     private void Update()
     {
         // lateral movement
