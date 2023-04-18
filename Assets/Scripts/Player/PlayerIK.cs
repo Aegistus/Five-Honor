e32262bd71ef7521872f1f389d10a55bba26b693
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-04-18 18:41:59 -0400

    Fix Weapon Rotation and Offset in Hand

--- Assets/Scripts/Player/PlayerIK.cs
+++ Assets/Scripts/Player/PlayerIK.cs
@@ -2,25 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum HandParent
+{
+    None, LeftHand, RightHand
+}
+
 public class PlayerIK : MonoBehaviour
 {
-    [SerializeField] Transform leftHandTarget;
+    [SerializeField] WeaponIK currentWeapon;
+    [SerializeField] Transform rightHand;
+    [SerializeField] Transform leftHand;
 
+    Transform WeaponTransform => currentWeapon.transform;
+    HandParent weaponParent = HandParent.None;
+
+    bool ik;
     Animator anim;
+    PlayerMovement movement;
 
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        movement = GetComponentInParent<PlayerMovement>();
+        movement.OnStanceChange += WeaponChangeHands;
+    }
+
+    private void Start()
+    {
+        WeaponChangeHands(StanceType.Passive);   
+    }
+
+    private void WeaponChangeHands(StanceType newStance)
+    {
+        if (newStance == StanceType.Combat)
+        {
+            ik = true;
+            WeaponTransform.SetParent(rightHand);
+            weaponParent = HandParent.RightHand;
+            WeaponTransform.localRotation = Quaternion.Euler(currentWeapon.rightHandRotation);
+            WeaponTransform.localPosition = currentWeapon.rightHandOffset;
+        }
+        else if (newStance == StanceType.Passive)
+        {
+            ik = false;
+            WeaponTransform.SetParent(leftHand);
+            weaponParent = HandParent.LeftHand;
+            WeaponTransform.localRotation = Quaternion.Euler(currentWeapon.leftHandRotation);
+            WeaponTransform.localPosition = currentWeapon.leftHandOffset;
+        }
+    }
+
+    private void Update()
+    {
+        if (weaponParent == HandParent.LeftHand)
+        {
+            WeaponTransform.localRotation = Quaternion.Euler(currentWeapon.leftHandRotation);
+            WeaponTransform.localPosition = currentWeapon.leftHandOffset;
+        }
+        if (weaponParent == HandParent.RightHand)
+        {
+            WeaponTransform.localRotation = Quaternion.Euler(currentWeapon.rightHandRotation);
+            WeaponTransform.localPosition = currentWeapon.rightHandOffset;
+        }
     }
 
     private void OnAnimatorIK(int layerIndex)
     {
-        if (leftHandTarget != null)
+        if (WeaponTransform != null && ik)
         {
             anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
             anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
-            anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTarget.position);
-            anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTarget.rotation);
+            anim.SetIKPosition(AvatarIKGoal.LeftHand, WeaponTransform.position);
+            anim.SetIKRotation(AvatarIKGoal.LeftHand, WeaponTransform.rotation);
         }
     }
 }
---
+++ Assets/Scripts/WeaponIK.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponIK : MonoBehaviour
+{
+    public Vector3 rightHandOffset;
+    public Vector3 rightHandRotation;
+    public Vector3 leftHandOffset;
+    public Vector3 leftHandRotation;
+}
