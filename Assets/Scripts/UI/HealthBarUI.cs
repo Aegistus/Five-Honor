20efb301570e31e20cbadc36a7dac3807cdb129b
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-05-02 20:06:05 -0400

    Make Health Bars Look At Camera

--- Assets/Scripts/UI/HealthBarUI.cs
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -10,6 +10,7 @@ public class HealthBarUI : MonoBehaviour
 
     AgentHealth agentHealth;
     RawImage barImage;
+    Transform mainCam;
 
 
     private void Start()
@@ -17,6 +18,7 @@ public class HealthBarUI : MonoBehaviour
         agentHealth = GetComponentInParent<AgentHealth>();
         agentHealth.OnDamageTaken += UpdateHealthBar;
         barImage = healthBarTransform.GetComponent<RawImage>();
+        mainCam = Camera.main.transform;
         UpdateHealthBar();
     }
 
@@ -25,6 +27,11 @@ public class HealthBarUI : MonoBehaviour
         float percentHealth = agentHealth.CurrentHealth / agentHealth.MaxHealth;
         healthBarTransform.localScale = new Vector2(percentHealth, 1);
         barImage.color = healthGradient.Evaluate(1 - percentHealth);
+    }
+
+    private void Update()
+    {
+        transform.LookAt(mainCam);
     }
 
 }
